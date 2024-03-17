a97434b14bf49ecc66841323514c67ca5fb81a93
21127615 <bghuy2003@gmail.com>
2024-03-17 15:29:38 +0700

    apply repository design pattern

--- src/main/java/vn/bachgiahuy/laptopshop/controller/UserController.java
+++ src/main/java/vn/bachgiahuy/laptopshop/controller/UserController.java
@@ -3,16 +3,20 @@ package vn.bachgiahuy.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.bachgiahuy.laptopshop.domain.User;
 
 import vn.bachgiahuy.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,8 +24,22 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String page = this.userService.handleHelloWorld();
-        model.addAttribute("eric", page);
+        model.addAttribute("eric", "user");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getCreateUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "/admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUser(Model model, @ModelAttribute("newUser") User newUser) {
+        System.out.println(newUser.toString());
+        User savedUser = this.userService.saveUser(newUser);
+        System.out.println(savedUser);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/vn/bachgiahuy/laptopshop/service/UserService.java
+++ src/main/java/vn/bachgiahuy/laptopshop/service/UserService.java
@@ -2,9 +2,22 @@ package vn.bachgiahuy.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.bachgiahuy.laptopshop.domain.User;
+import vn.bachgiahuy.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHelloWorld() {
         return "eric";
+    }
+
+    public User saveUser(User user) {
+        return this.userRepository.save(user);
     }
 }
