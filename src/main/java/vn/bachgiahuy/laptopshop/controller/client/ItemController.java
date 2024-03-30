fa947f52f385385f20eeabad407f9c5279aeb1d5
21127615 <bghuy2003@gmail.com>
2024-03-30 14:19:34 +0700

    basic product detail page

--- src/main/java/vn/bachgiahuy/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/bachgiahuy/laptopshop/controller/client/ItemController.java
@@ -5,10 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.bachgiahuy.laptopshop.domain.Product;
+import vn.bachgiahuy.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getDetailPage(Model model, @PathVariable long id) {
+        Product product = this.productService.getProductById(id);
+        model.addAttribute("id", id);
+        model.addAttribute("product", product);
         return "/client/product/detail";
     }
 }
